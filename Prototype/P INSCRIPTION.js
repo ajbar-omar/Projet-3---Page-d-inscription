d4b9fa98625501f7775c232825abcd893a703243
ajbar-omar <ajbar.omar.dev@gmail.com>
2021-04-08 21:47:15 +0200

    .

---
+++ Prototype/P INSCRIPTION.js
@@ -0,0 +1,10 @@
+
+    function om() {
+var text=document.getElementById("Nom").value;
+document.getElementById ("p").innerHTML+="<b>Nom:</b>&nbsp;"+text+"<br>";
+    
+        var text=document.getElementById("Prénom").value;
+        document.getElementById ("p").innerHTML+="<b>Prénome:</b>&nbsp;"+text+"<br>";
+            }
+
+        
